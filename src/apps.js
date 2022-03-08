7ef2401a610422565b85c74b6bad8e7afe48567c
sammieshing <sammiestarr@gmail.com>
2022-03-09 00:01:59 +1100

    including the search icon and streamlining code

--- src/apps.js
+++ src/apps.js
@@ -49,14 +49,20 @@ displayDate.innerHTML = `${day}, ${date} ${month} ${year} ${hours}:${minutes}`;
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#display-temp");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp); 
   let cityElement = document.querySelector("#city");
+  let weatherCondition = document.querySelector("#display-cond");
+  let iconElement = document.querySelector("#icon");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp); 
   cityElement.innerHTML = response.data.name; 
+  weatherCondition.innerHTML = response.data.weather[0].description;
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].icon);
 }
 
 let apiKey = "9acc1ba10c354b22cdd73fddfa649e54"
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=${apiKey}&units=${units}`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(displayTemperature)
 
